71a78096c0b363196104d8e20105de02d0cb7630
Yinhang Liu <yinhangx.liu@intel.com>
2020-12-13 14:54:21 +0800

    gl-yuv420-fastmap: fix the local_size_y of UV channel

--- modules/gles/gl_geomap_handler.cpp
+++ modules/gles/gl_geomap_handler.cpp
@@ -652,7 +652,7 @@ FastMapYUV420::fix_uv_parameters (const VideoBufferInfo &in_info)
 
     GLGroupsSize groups_size;
     groups_size.x = XCAM_ALIGN_UP (std_valid_width, 4) / 4;
-    groups_size.y = XCAM_ALIGN_UP (std_area.height / 2, 8) / 8;
+    groups_size.y = XCAM_ALIGN_UP (std_area.height / 2, 4) / 4;
     groups_size.z = 1;
     _shader_uv->set_groups_size (groups_size);
 
