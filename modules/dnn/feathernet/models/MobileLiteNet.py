5b837a4c9e81dd47232a61c7b66872c8c016e50f
Zong Wei <wei.zong@intel.com>
2022-09-19 10:02:30 +0800

    Revert "Latest"
    This reverts commit 4b7cfe682a5a8c65700b32978354ef83e5e3fb54.

---
+++ modules/dnn/feathernet/models/MobileLiteNet.py
@@ -0,0 +1,207 @@
+'''
+The code of class InvertedResidual and class MobileLiteNet was taken from
+https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+and modified by Zhang Peng. The copyright notice for these codes is as follows:
+
+BSD 3-Clause License
+
+Copyright (c) Soumith Chintala 2016,
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import numpy as np
+import math
+         
+# reference form : https://github.com/moskomule/senet.pytorch  
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=8):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                nn.ReLU(inplace=True),
+                nn.Linear(channel // reduction, channel),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+    
+# For licensing information pertaining to this class, see the comments at the top of this file.
+class InvertedResidual(nn.Module):
+    expansion = 1
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(InvertedResidual, self).__init__()
+        self.expansion = 6
+        self.conv1 = nn.Conv2d(inplanes, inplanes*self.expansion, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(inplanes*self.expansion)
+        self.conv2 = nn.Conv2d(inplanes*self.expansion, inplanes*self.expansion, kernel_size=3, stride=stride,
+                               padding=1, groups=inplanes, bias=False)
+        self.bn2 = nn.BatchNorm2d(inplanes*self.expansion)
+        self.conv3 = nn.Conv2d(inplanes*self.expansion, planes , kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU6(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+        
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = out
+        
+        return out
+    
+# For licensing information pertaining to this class, see the comments at the top of this file.
+class MobileLiteNet(nn.Module):
+    def __init__(self,block,layers,num_classes = 2,se = False):
+        
+        super(FeatherNet, self).__init__()
+        self.se = se
+        self.channels = [32, 16, 32, 48, 64]
+        self.inplanes = self.channels[1]
+        self.conv1 = nn.Conv2d(3, self.channels[0], kernel_size=3, stride=2, padding=1,bias=False)
+        self.bn1 = nn.BatchNorm2d(self.channels[0])
+        self.relu = nn.ReLU6(inplace=True)
+        self.conv2 = nn.Conv2d(self.channels[0], self.channels[1], kernel_size=1,bias=False)
+        self.bn2 = nn.BatchNorm2d(self.channels[1])
+        
+        self.layer1 = self._make_layer(block, self.channels[1], layers[0], stride=2)
+        self.layer2 = self._make_layer(block, self.channels[2], layers[1], stride=2)
+        self.layer3 = self._make_layer(block, self.channels[3], layers[2], stride=2)
+        self.layer4 = self._make_layer(block, self.channels[4], layers[3], stride=2)
+        self.final_DW = nn.Conv2d(self.channels[4], self.channels[4], kernel_size=4, 
+                                  groups=self.channels[4], bias=False)
+        self.do = nn.Dropout(0.2) 
+        self.linear = nn.Linear(self.channels[4]*16, num_classes)
+        if self.se:
+            self.layer1_se1= SELayer(self.channels[1])
+            self.layer2_se2= SELayer(self.channels[2])
+            self.layer3_se3= SELayer(self.channels[3])
+            self.layer4_se4= SELayer(self.channels[4])
+        
+        self._initialize_weights()
+        
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 :
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, self.inplanes ,
+                          kernel_size=3, stride=stride,padding=1, groups=self.inplanes, bias=False),
+                nn.BatchNorm2d(self.inplanes),
+                nn.Conv2d(self.inplanes, planes , kernel_size=1, bias=False)
+            )
+
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        if self.se:
+            x = self.layer1_se1(self.layer1(x))
+            x = self.layer2_se2(self.layer2(x))
+            x = self.layer3_se3(self.layer3(x))
+            x = self.layer4_se4(self.layer4(x))
+        else:
+            x = self.layer1(x)
+            x = self.layer2(x)
+            x = self.layer3(x)
+            x = self.layer4(x)
+
+        x = self.final_DW(x)
+        x = x.view(x.size(0), -1)
+
+        x = self.do(x)
+        x = self.linear(x)
+        return x
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+def MobileLiteNet54( **kwargs):
+    model = MobileLiteNet(InvertedResidual, [4, 4, 6, 3], num_classes=2,se = False, **kwargs)
+    return model 
+def MobileLiteNet54_se( **kwargs):
+    model = MobileLiteNet(InvertedResidual, [4, 4, 6, 3], num_classes=2,se = True, **kwargs)
+    return model
+def MobileLiteNet102( **kwargs):
+    model = MobileLiteNet(InvertedResidual, [3, 4, 23, 3], num_classes=2,se = False, **kwargs)
+    return model 
+def MobileLiteNet105_se( **kwargs):
+    model = MobileLiteNet(InvertedResidual, [4, 4, 23, 3], num_classes=2,se = True, **kwargs)
+    return model 
+def MobileLiteNet153( **kwargs):
+    model = MobileLiteNet(InvertedResidual, [3, 8, 36, 3], num_classes=2,se = False, **kwargs)
+    return model 
+def MobileLiteNet156_se( **kwargs):
+    model = MobileLiteNet(InvertedResidual, [4, 8, 36, 3], num_classes=2,se = True, **kwargs)
+    return model 
---
+++ modules/dnn/inference/dnn_object_detection.cpp
@@ -0,0 +1,240 @@
+/*
+ * dnn_object_detection.cpp -  object detection
+ *
+ *  Copyright (c) 2019 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Zong Wei <wei.zong@intel.com>
+ */
+
+#include <inference_engine.hpp>
+
+#include "dnn_object_detection.h"
+
+using namespace std;
+using namespace InferenceEngine;
+
+namespace XCam {
+
+DnnObjectDetection::DnnObjectDetection (DnnInferConfig& config)
+    : DnnInferenceEngine (config)
+{
+    XCAM_LOG_DEBUG ("DnnObjectDetection::DnnObjectDetection");
+    set_output_layer_type ("DetectionOutput");
+}
+
+DnnObjectDetection::~DnnObjectDetection ()
+{
+
+}
+
+XCamReturn
+DnnObjectDetection::set_output_layer_type (const char* type)
+{
+    _output_layer_type.insert (DnnOutputLayerType::value_type (DnnInferObjectDetection, type));
+    return XCAM_RETURN_NO_ERROR;
+}
+
+XCamReturn
+DnnObjectDetection::get_model_input_info (DnnInferInputOutputInfo& info)
+{
+    if (NULL == _ie.ptr ()) {
+        XCAM_LOG_ERROR ("Please create inference engine");
+        return XCAM_RETURN_ERROR_ORDER;
+    }
+
+    InputsDataMap inputs_info (_network.getInputsInfo ());
+    InputInfo::Ptr input_info = nullptr;
+
+    int id = 0;
+    InferenceEngine::Precision precision;
+    for (auto & item : inputs_info) {
+        if (item.second->getInputData()->getTensorDesc().getDims().size() == 4) {
+            input_info = item.second;
+            XCAM_LOG_DEBUG ("Batch size is: %d", _network.getBatchSize());
+            precision = Precision::U8;
+            item.second->setPrecision(Precision::U8);
+        } else if (item.second->getInputData()->getTensorDesc().getDims().size() == 2) {
+            precision = Precision::FP32;
+            item.second->setPrecision(Precision::FP32);
+            if ((item.second->getTensorDesc().getDims()[1] != 3 && item.second->getTensorDesc().getDims()[1] != 6)) {
+                XCAM_LOG_ERROR ("Invalid input info. Should be 3 or 6 values length");
+                return XCAM_RETURN_ERROR_PARAM;
+            }
+        }
+
+        info.width[id] = inputs_info[item.first]->getTensorDesc().getDims()[3];
+        info.height[id] = inputs_info[item.first]->getTensorDesc().getDims()[2];
+        info.channels[id] = inputs_info[item.first]->getTensorDesc().getDims()[1];
+        info.object_size[id] = inputs_info[item.first]->getTensorDesc().getDims()[0];
+        info.precision[id] = convert_precision_type (precision);
+        id++;
+    }
+
+    info.batch_size = get_batch_size ();
+    info.numbers = inputs_info.size ();
+
+    return XCAM_RETURN_NO_ERROR;
+}
+
+XCamReturn
+DnnObjectDetection::set_model_input_info (DnnInferInputOutputInfo& info)
+{
+    XCAM_LOG_DEBUG ("DnnObjectDetection::set_model_input_info");
+
+    if (NULL == _ie.ptr ()) {
+        XCAM_LOG_ERROR ("Please create inference engine");
+        return XCAM_RETURN_ERROR_ORDER;
+    }
+
+    InputsDataMap inputs_info (_network.getInputsInfo ());
+    if (info.numbers != inputs_info.size ()) {
+        XCAM_LOG_ERROR ("Input size is not matched with model info numbers %d !", info.numbers);
+        return XCAM_RETURN_ERROR_PARAM;
+    }
+
+    int idx = 0;
+    for (auto & in : inputs_info) {
+        Precision precision = convert_precision_type (info.precision[idx]);
+        in.second->setPrecision (precision);
+        Layout layout = convert_layout_type (info.layout[idx]);
+        in.second->setLayout (layout);
+        idx ++;
+    }
+
+    return XCAM_RETURN_NO_ERROR;
+}
+
+XCamReturn
+DnnObjectDetection::get_model_output_info (DnnInferInputOutputInfo& info)
+{
+    if (NULL == _ie.ptr ()) {
+        XCAM_LOG_ERROR ("Please create inference engine");
+        return XCAM_RETURN_ERROR_ORDER;
+    }
+
+    int idx = 0;
+    std::string output_name;
+    OutputsDataMap outputs_info (_network.getOutputsInfo ());
+    DataPtr output_info;
+    for (const auto& out : outputs_info) {
+        if (output_name.empty ()) {
+            output_name = out.first;
+        }
+
+        output_info = out.second;
+        if (output_info.get ()) {
+            const InferenceEngine::SizeVector output_dims = output_info->getTensorDesc().getDims();
+
+            info.width[idx]    = output_dims[0];
+            info.height[idx]   = output_dims[1];
+            info.channels[idx] = output_dims[2];
+            info.object_size[idx] = output_dims[3];
+            info.precision[idx] = convert_precision_type (output_info->getPrecision());
+            info.layout[idx] = convert_layout_type (output_info->getLayout());
+            info.data_type[idx] = DnnInferDataTypeNonImage;
+            info.format[idx] = DnnInferImageFormatUnknown;
+            info.batch_size = idx + 1;
+            info.numbers = outputs_info.size ();
+        } else {
+            XCAM_LOG_ERROR ("Get output info error!");
+            return XCAM_RETURN_ERROR_UNKNOWN;
+        }
+        idx ++;
+    }
+    return XCAM_RETURN_NO_ERROR;
+}
+
+XCamReturn
+DnnObjectDetection::set_model_output_info (DnnInferInputOutputInfo& info)
+{
+    if (NULL == _ie.ptr ()) {
+        XCAM_LOG_ERROR ("Please create inference engine");
+        return XCAM_RETURN_ERROR_ORDER;
+    }
+
+    OutputsDataMap outputs_info (_network.getOutputsInfo ());
+    if (info.numbers != outputs_info.size ()) {
+        XCAM_LOG_ERROR ("Output size is not matched with model!");
+        return XCAM_RETURN_ERROR_PARAM;
+    }
+
+    int idx = 0;
+    for (auto & out : outputs_info) {
+        Precision precision = convert_precision_type (info.precision[idx]);
+        out.second->setPrecision (precision);
+        Layout layout = convert_layout_type (info.layout[idx]);
+        out.second->setLayout (layout);
+        idx++;
+    }
+
+    return XCAM_RETURN_NO_ERROR;
+}
+
+XCamReturn
+DnnObjectDetection::get_bounding_boxes (const float* result_ptr,
+                                        const uint32_t idx,
+                                        std::vector<Vec4i> &boxes,
+                                        std::vector<int32_t> &classes)
+{
+    if (NULL == _ie.ptr ()) {
+        XCAM_LOG_ERROR ("Please create inference engine");
+        return XCAM_RETURN_ERROR_ORDER;
+    }
+
+    if (!result_ptr) {
+        XCAM_LOG_ERROR ("Inference results error!");
+        return XCAM_RETURN_ERROR_PARAM;
+    }
+
+    DnnInferInputOutputInfo output_infos;
+    get_model_output_info (output_infos);
+
+    uint32_t image_width = get_input_image_width (idx);
+    uint32_t image_height = get_input_image_height (idx);
+    uint32_t max_proposal_count = output_infos.channels[idx];
+    uint32_t object_size = output_infos.object_size[idx];
+
+    uint32_t box_count = 0;
+    for (uint32_t cur_proposal = 0; cur_proposal < max_proposal_count; cur_proposal++) {
+        float image_id = result_ptr[cur_proposal * object_size + 0];
+        if (image_id < 0) {
+            break;
+        }
+
+        float label = result_ptr[cur_proposal * object_size + 1];
+        float confidence = result_ptr[cur_proposal * object_size + 2];
+        float xmin = result_ptr[cur_proposal * object_size + 3] * image_width;
+        float ymin = result_ptr[cur_proposal * object_size + 4] * image_height;
+        float xmax = result_ptr[cur_proposal * object_size + 5] * image_width;
+        float ymax = result_ptr[cur_proposal * object_size + 6] * image_height;
+
+        if (confidence > 0.5) {
+            classes.push_back(static_cast<int32_t>(label));
+            boxes.push_back (Vec4i ( static_cast<int32_t>(xmin),
+                                     static_cast<int32_t>(ymin),
+                                     static_cast<int32_t>(xmax - xmin),
+                                     static_cast<int32_t>(ymax - ymin) ));
+
+            XCAM_LOG_DEBUG ("Proposal:%d label:%d confidence:%f", cur_proposal, classes[box_count], confidence);
+            XCAM_LOG_DEBUG ("Boxes[%d] {%d, %d, %d, %d}",
+                            box_count, boxes[box_count][0], boxes[box_count][1],
+                            boxes[box_count][2], boxes[box_count][3]);
+            box_count++;
+        }
+    }
+    return XCAM_RETURN_NO_ERROR;
+}
+
+}  // namespace XCam
